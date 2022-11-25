c8915bc39a1233c2f23d3a3a29e75d5676b4fba1
Laurent Sauvé <lsauve@iweb.com>
2022-11-25 10:04:23 -0500

    Adding and ajusting to deploy ubuntu 22.04 images
    Also added a script to reduce the size of the images build

---
+++ images/capi/ansible/roles/sysprep/files/zerodisk.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Info: Zero out the free space to save space"
+
+AVAIL=$(df --block-size=4K /|tail -1|awk '{print $4}')
+
+dd if=/dev/zero of=/EMPTY conv=fsync bs=4k count=${AVAIL}
+rm -f /EMPTY
+
+unset HISTFILE
+
+sync
